24d09f51c6090c604f6aa5c002afda0bec18e8a9
Santopelufincho <juan.boschero@hotmail.com>
2015-08-07 08:20:20 -0300

    Added CredntialCheck class

---
+++ DeptDets/CredentialCheck.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DeptDets
+{
+    class CredentialCheck
+    {
+        string strCredentialCheck;
+
+        public CredentialCheck(string strCC)
+        {
+            strCredentialCheck = strCC;
+        }
+
+  public string[,] Checkfile(ref int nLength)
+        {
+            string[,] arrRows = new string[500, 20];
+
+            try
+            {
+                using (StreamReader stmCheck = new StreamReader(strCredentialCheck))
+                {
+                    while (stmCheck.Peek() > 0)
+                    {
+
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+            return arrRows;
+        }*/
+    }
+}
