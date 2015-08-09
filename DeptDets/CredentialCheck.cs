f3d8a2f17fde6218fbb6e0d6a3d75c194d5f2978
PotatoProgrammer <anttonipykalisto@gmail.com>
2015-08-08 21:49:10 -0300

    CSV shenanigans changed

--- DeptDets/CredentialCheck.cs
+++ DeptDets/CredentialCheck.cs
@@ -16,9 +16,23 @@ namespace DeptDets
             strCredentialCheck = strCC;
         }
 
-  public string[,] Checkfile(ref int nLength)
+        public string[,] Checkfile(ref int nLength)
         {
-            string[,] arrRows = new string[500, 20];
+            var reader = new StreamReader(File.OpenRead(@"C:\test.csv"));
+            
+            List<string> listA = new List<string>();
+            List<string> listB = new List<string>();
+            
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(';');
+
+                listA.Add(values[0]);
+                listB.Add(values[1]);
+            }
+            
+            string[,] arrRows = new string[2, 40];
 
             try
             {
@@ -31,7 +45,7 @@ namespace DeptDets
 
                     while (stmCheck.Peek() > 0)
                     {
-
+                        
                     }
                 }
             }
